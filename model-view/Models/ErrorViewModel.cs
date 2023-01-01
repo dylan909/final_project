cc1f1f84f90a458e1255cf8cb2c6a85185b9f279
dylan909 <dylanmichealkennedy6@gmail.com>
2023-01-01 14:17:15 +0000

    First Commit

---
+++ model-view/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace model_view.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
+
